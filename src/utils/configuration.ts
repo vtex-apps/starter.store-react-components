577499e234da51e15ef30591cec30e20b9dabe6e
Rafael Sangalli <rafael.sangalli@vtex.com.br>
2021-08-25 18:39:18 -0300

    Fixes lint issue

--- src/utils/configuration.ts
+++ src/utils/configuration.ts
@@ -1,18 +1,17 @@
-
-import * as configFile from '../config.json'
+import * as configFile from '../config.json';
 interface Configuration {
   serverUrl: string;
 }
 
-function getServerBaseUrl() : string {
+function getServerBaseUrl(): string {
   if (process.env.USE_LOCAL_SERVER === 'true') {
-    return configFile.appBackendBaseUrl.localServer
+    return configFile.appBackendBaseUrl.localServer;
   } else {
-    return configFile.appBackendBaseUrl[process.env.NODE_ENV]
+    return configFile.appBackendBaseUrl[process.env.NODE_ENV];
   }
 }
 
-let config: Configuration= {
+let config: Configuration = {
   serverUrl: `${getServerBaseUrl()}/graphql`,
 };
 
